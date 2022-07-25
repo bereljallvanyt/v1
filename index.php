f9fb72f040069b5da425f0ec59196dc20845e623
3gomen <gergofekete.23@gmail.com>
2022-07-25 23:33:05 +0200

    start php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html class="no-js" lang="hu">
+
+<?php
+    
+    $title = 'Carters Logistics';
+    
+    include 'head.php';
+    
+    echo '<body class=" has-topbar">';
+    
+    include 'nav.php';
+    include 'body.php';
+    
+?>
